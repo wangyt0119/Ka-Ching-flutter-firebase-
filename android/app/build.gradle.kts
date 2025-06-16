a2f829cb58985d00b124cff6a12bf3399e34c4a9
YunXiTan <tyunxi03@gmail.com>
2025-06-17 02:34:01 +0800

    Revert "build gradle back"
    This reverts commit 50ed4a9de5675d61886b144b995c46960275139e.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,7 @@ plugins {
 android {
     namespace = "com.example.kaching"
     compileSdk = flutter.compileSdkVersion
-    //ndkVersion = "27.0.12077973"
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -28,8 +27,7 @@ android {
         applicationId = "com.example.kaching"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        //minSdk = 23
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
